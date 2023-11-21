66eab4de5d83a9ef61eafb73eb7999372838f82a
Lorenzo <61066767+CafeurOff@users.noreply.github.com>
2023-11-21 16:25:52 +0100

    Add builtin

---
+++ cd.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduthill <lduthill@42perpignan.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 13:09:55 by lduthill          #+#    #+#             */
+/*   Updated: 2023/11/21 13:50:07 by lduthill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* NOTES :
+*   cd ~ : Back to $HOME
+*   cd $ENV : if the variable is a right path cd to the path.
+*/
+
+
+/* ft_cd()
+*	Function for change directory
+*	@data : struct with all the data
+*	@pars : struct with all the parsed data
+*/
+
+void	ft_cd(t_data *data, t_pars *pars)
+{
+	int	i;
+
+	i = 0;
+	while (pars->args[i])
+	{
+		if (pars->args[i][0] == '~')
+			ft_cd_home(data);
+		else
+			ft_cd_path(pars);
+		i++;
+	}
+}
+/* ft_cd_home()
+*  Function for change directory to $HOME
+*  @data : struct with all the data
+*/
+
+void	ft_cd_home(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->env[i].id)
+	{
+		if (ft_strncmp(data->env[i].id, "HOME", ft_strlen(data->env[i].id)) == 0)
+		{
+			chdir(data->env[i].value);
+			return ;
+		}
+		i++;
+	}
+}
+
+/* ft_cd_path()
+*  Function for change directory to a path
+*  @data : struct with all the data
+*  @pars : struct with all the parsed data
+*/
+  char cwd[256];
+
+void	ft_cd_path(t_pars *pars)
+{
+	int	i;
+
+	i = 0;
+	while (pars->args[i])
+	{
+		if (chdir(pars->args[i]) == -1)
+		{
+			printf("minishell: cd: %s: No such file or directory\n",
+				pars->args[i]);
+			return ;
+		}
+		else
+			getcwd(cwd, sizeof(cwd));
+    		printf("Current working dir: %s\n", cwd);
+		i++;
+	}
+}
---
+++ export.c
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduthill <lduthill@42perpignan.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 15:14:46 by lduthill          #+#    #+#             */
+/*   Updated: 2023/11/21 13:42:25 by lduthill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* NOTE :
+*	Si export avec un = alors afficher dans env sinon dans export
+*/
+
+void	ft_export(t_pars *pars, t_data *data, int i)
+{
+	int	j;
+	int	k;
+
+	j = 0;
+	if (!pars->args)
+	{
+		ft_print_export(data->env);
+		return ;
+	}
+	while (pars[i].args[j])
+	{
+		k = ft_id_index(data, pars[i].args[j]);
+		if (ft_id_exist(data, pars[i].args[j]) == 0)
+			ft_setid(data, pars[i].args[j]);
+		if (ft_strchr(pars[i].args[j], 61) != NULL)
+		{
+			data->env[k].set = 1;
+			if (data->env[k].value)
+				free(data->env[k].value);
+			data->env[k].value = ft_strdup(ft_strchr(pars[i].args[j], 61) + 1);
+		}
+		j++;
+	}
+}
+
+void	ft_setid(t_data *data, char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[j] && str[j] != 61)
+		j++;
+	while (data->env[i].id)
+		i++;
+	data->env[i].id = malloc(sizeof(char) * (j + 1));
+	if (!data->env[i].id)
+		return ;
+	j = 0;
+	while (str[j] != 61 && str[j])
+	{
+		data->env[i].id[j] = str[j];
+		j++;
+	}
+	data->env[i].id[j] = '\0';
+}
+
+/*  ft_setenv()
+*   Function for set a new value to an env variable
+*   Exemple : ft_setenv("PATH", "/bin/ls", env);
+*   Return : ft_getenv("PATH", env) == "/bin/ls"
+*/
+
+void	ft_setenv(char *find, char *str, t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i].id)
+	{
+		if (ft_strncmp(find, env[i].id, ft_strlen(find)) == 0)
+		{
+			if (str[0] == '\0')
+			{
+				env[i].value = NULL;
+				return ;
+			}
+			free(env[i].value);
+			env[i].value = ft_strdup(str);
+			return ;
+		}
+		else
+			return ; // Error message with free
+		i++;
+	}
+}
+
+/*	ft_print_export()
+*	Function for print all env variable
+*/
+
+void	ft_print_export(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i].id)
+	{
+		if (env[i].set == 0)
+			printf("declare -x %s\n", env[i].id);
+		else
+			printf("declare -x %s=\"%s\"\n", env[i].id, env[i].value);
+		i++;
+	}
+}
+int		ft_id_exist(t_data *data, char *str)
+{
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	while (str[i] != 61 && str[i])
+		i++;
+	while (data->env[j].id)
+	{
+		if (ft_strncmp(data->env[j].id, str, i) == 0)
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+int		ft_id_index(t_data *data, char *str)
+{
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	while (str[i] != 61 && str[i])
+		i++;
+	while (data->env[j].id)
+	{
+		if (ft_strncmp(data->env[j].id, str, i) == 0)
+			return (j);
+		j++;
+	}
+	return (j);
+}
